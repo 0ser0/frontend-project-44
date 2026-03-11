00705cc40c85c4bd2f15168b5368f64e4c18a185
0ser0 <0sercos0@gmail.com>
2026-03-11 19:25:02 +1000

    Fix function src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -18,8 +18,12 @@ const isPrimeNumber = (num) => {
     }
 
     for (let i = 3; i <= Math.sqrt(num); i += 2) {
-        if (num % i === 0) return false
+        if (num % i === 0){
+        return false
+        }
     }
+
+    return true
 }
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
